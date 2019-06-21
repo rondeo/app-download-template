691306ced7548236c5589ee16c50df728438df83
ouzuyu <oupenguin@gmail.com>
2019-06-21 17:02:45 +0800

    make build.sh executable

--- build.sh
+++ build.sh
@@ -3,6 +3,7 @@ yarn stdver
 
 yarn build
 
+# git remote add github https://$GITHUB_TOKEN@github.com/FEMessage/app-download.git > /dev/null 2>&1
 git remote add github https://$GITHUB_TOKEN@github.com/FEMessage/app-download.git > /dev/null 2>&1
 git push github HEAD:master --follow-tags
 
